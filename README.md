068b881d523329408058745001ba957f5f0cdab2
Max Moulds@dev2 <maxmoulds@users.noreply.github.com>
2018-05-25 19:15:21 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DockerInstall
+
+Simple shell script for installing Docker/DockerCompose on Ubuntu 17.10 artful   
---
+++ instalDocker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Updating pre docker install"
+
+sudo apt-get update
+
+sudo apt-get install \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+sudo apt-get update
+
+echo "Installing Docker"
+
+sudo apt-get install docker-ce
+
+echo "Starting Docker"
+
+sudo systemctl start docker
+
+sudo systemctl enable docker 
+
+echo "Giving current User Docker permissions"
+
+sudo usermod -aG docker $USER
+
+echo "Installing Docker Compose"
+
+sudo pip install docker-compose
+
+docker-compose --version
